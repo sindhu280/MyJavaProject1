543b44fa79d825841a995aea79c04eaafc0a5325
Sindhu <151599499+sindhu280@users.noreply.github.com>
2023-11-21 11:15:01 +0530

    Update Welcome.java

--- src/com/edubridge/app1/Welcome.java
+++ src/com/edubridge/app1/Welcome.java
@@ -2,7 +2,6 @@ package com.edubridge.app1;
 public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("welcome to java");
-		System.out.println("welcome to python");
 		System.out.println("welcome to spring");
 	}
 

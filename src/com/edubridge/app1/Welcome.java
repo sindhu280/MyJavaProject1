7430e1f6e662c0d5bba6e76884abc4406b07c4aa
Sindhu228 <psindhu7839@gmail.com>
2023-11-21 12:03:20 +0530

    welcome class is added

--- src/com/edubridge/app1/Welcome.java
+++ src/com/edubridge/app1/Welcome.java
@@ -2,6 +2,7 @@ package com.edubridge.app1;
 public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("welcome to java");
+		System.out.println("wwlcome to python");
 	}
 
 }
